5c4c6e6dffdd0dd9da86ffa26d4ef28106708821
JavadJananeh <150336284+JavadJananeh@users.noreply.github.com>
2024-02-16 11:30:37 +0100

    NewMian

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import media from "./media/pic.jpg";
+import anime from 'animejs'
 
 
 const img = document.createElement('img');
 document.body.append(img);
 img.src = media
+anime ({
+    targets: img,
+    rotate: 360
+})
